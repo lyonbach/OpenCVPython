834017e4db628e0efcb0b156a7aa3a30d234594e
Bahadir <arslan.bah@gmail.com>
2020-04-12 09:22:15 +0200

    lb_opencv_helpers :: Added resize function.

--- shared_python_modules/lb_opencv_helpers.py
+++ shared_python_modules/lb_opencv_helpers.py
@@ -4,9 +4,29 @@ This module is intended to be a bridge between different image processing relate
 when dealing intermediate tasks.
 """
 
+import cv2
 import numpy as np
 import dlib
 
+# General.
+def resize(source, width=None, height=None):
+
+    if not (width or height):
+        print("[lb_helpers][ERROR]: Width or height needed!")
+        return
+
+    if not width or not height:
+        source_height, source_width = source.shape[:2]
+
+        if width:
+            height = source_height * width / source_width
+        else:
+            width = source_width * height / source_height
+
+    return cv2.resize(source, (int(width), int(height)))
+
+
+# dlib related.
 def dlib_rectangle_to_bounding_box(rectangle):
 
     """
